6ef181557a3660128b177b639e7d1d5c8c573327
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-08-30 16:03:41 -0300

    ADO I - Calculadora de IMC

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Test_Aula_LSW
- Atividades em aula de Linguagens de Script para Web
+ADO I
---
+++ script.js
@@ -0,0 +1,6 @@
+//IMC = PESO / (ALTURA)²
+let Peso=parseFloat(prompt("Qual o seu peso?"));
+let Altura=parseFloat(prompt("Qual a sua altura?(Divida com o ponto[ . ]"));
+let IMC=(Peso/(Altura*Altura));
+
+window.alert("Seu IMC é de: "+IMC);
