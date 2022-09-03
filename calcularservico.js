79e33f50b27f068bcb3b18ad20381af735e5a403
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-09-02 22:32:20 -0300

    Vetores, funções e template string

---
+++ calcularservico.js
@@ -0,0 +1,17 @@
+/*
+    CALCULADORA DE SERVIÇOS
+-Criar uma calculadora de valor a ser cobrado do cliente
+-O valor do serviço pode variar conforme o prazo
+-Os valores e prazos devem ser armazenados em uma matriz.
+-O cálculo deve ficar obrigatoriamente em função
+*/
+
+let service=parseInt(prompt("Qual o serviço que você quer?[1][2][3]"));
+let hour=parseInt(prompt("Em quantas horas?[24][48][72]"));
+
+
+
+let valores=['$100','$200','$300','$400','$500','$600','$700','$800','$900'];
+console.log(valores.length);
+
+"Vetores, funções e template string"
