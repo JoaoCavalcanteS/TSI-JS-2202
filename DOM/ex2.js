f0449d6891b9ca600296fb084537c17e69936b46
João Cavalcante <joaovitorcavalcante2012@gmail.com>
2022-09-11 23:20:08 -0300

    CalculadoraS

--- DOM/ex2.js
+++ DOM/ex2.js
@@ -1,18 +1,26 @@
-let bntCalcular=document.getElementById('calcular');
+let serviço=document.getElementById('servico');
+let prazo=document.getElementById('titulo');
+let resultado=document.getElementById('resultado');
 
-//esse método espera uma função e um parametro
+
+let tabela=[['$100','$200','$300','$400','$500','$600','$700','$800','$900']];
+
+
+if(validaEntrada(servico) && validaEntrada(prazo)){
+        resultado.innerText("O serviço ficará em: "+tabela[servico][prazo]);
+}
+else{
+    alert("ERRO");
+}
+
+/*function validaEntrada(dadoUsuario){
+    return dadoUsuario>= 0 && dadoUsuario<=2 ? true : false;
+}
+
+/*esse método espera uma função e um parametro
 bntCalcular.addEventListener('click', function(evento){
     evento.preventDefault();
-    console.log(evento);
-
-    //obter os valores de serviço e prazo
-});
-//faça o mesmo exercicio do serviços
-
-
-
-
-
+    console.log(evento);*/
 
 
 /*ou fazer assim
